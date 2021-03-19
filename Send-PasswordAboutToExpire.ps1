d9dcfc7bfb4feebfecefcf651539932b541499df
Rodrigo T Lima <45399246+RodrigoTL@users.noreply.github.com>
2021-03-19 14:02:32 -0300

    Update Send-PasswordAboutToExpire.ps1
    Working version, Report must be fixed

--- Send-PasswordAboutToExpire.ps1
+++ Send-PasswordAboutToExpire.ps1
@@ -1,26 +1,36 @@
-﻿
+
 $filterDate = [datetime]::Today.AddDays(+5)
 $filterDate2 = [datetime]::Today.AddDays(+1)
 
 $filterDate
+$filterDate2
 $UsersWithpasswordabouttoexpire = Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False } –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed","mail","extensionAttribute14" -SearchBase "OU=RSE,OU=HSDG,DC=hsdg-ad,DC=int" |
 Select-Object -Property "Displayname",SamAccountName,@{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}},"mail","extensionAttribute14" |
 Where-Object {$_.ExpiryDate -le $filterDate -and $_.ExpiryDate -ge $filterDate2}
 
 $UsersWithpasswordabouttoexpire|ft
+$UsersWithpasswordabouttoexpire.Count
 
+$report = $UsersWithpasswordabouttoexpire|Sort-Object -Property ExpiryDate |ft -AutoSize|Out-String
+
+Send-MailMessage -To rodrigo.lima-external@alianca.com.br -from Servicedesk-sao@hamburgsud.com -Subject "$($UsersWithpasswordabouttoexpire.Count) Usuários com senha expirando" -Body $report -SmtpServer smtp-rse.hsdg-ad.int -Encoding UTF32
 
     foreach ($User in $UsersWithpasswordabouttoexpire) {
 
-            [string[]]$to = @($($User.mail))
-            #[string[]]$to = @($($User.mail),$($User.extensionAttribute14))  
+            $to = @()
+            if($User.mail){
+                $to +=$User.mail}
+            if($User.extensionAttribute14){
+                $to +=$User.extensionAttribute14}
+            
+            $data = $($User.ExpiryDate.ToLocalTime)
             $subject = "Your password is about to expire | Sua senha está prestes a expirar"
             $body = @"
 <body style="font-family:Arial;">
 
 Por favor,<br>
 <br>
-A senha do usuario $($User.SamAccountName) vai expirar as $($User.ExpiryDate) (UTC-3) <br>
+A senha do usuario $($User.SamAccountName) vai expirar $([System.Threading.Thread]::CurrentThread.CurrentCulture = "pt-BR"; $User.ExpiryDate.DateTime) (UTC-3) <br>
 <br>
 Fa&ccedil;a a troca de senha o mais breve poss&iacute;vel, para n&atilde;o ter problemas de acesso<br>
 <br>
@@ -30,7 +40,7 @@ ________________________<br>
 <br>
 Please,<br>
 <br>
-User $($User.SamAccountName) password will expire at $($User.ExpiryDate) (UTC-3)<br>
+User $($User.SamAccountName) password will expire at $([System.Threading.Thread]::CurrentThread.CurrentCulture = "en-US"; $User.ExpiryDate.DateTime) (UTC-3)<br>
 <br>
 Change your password as soon as possible, so you will have no trouble to access<br>
 <br>
@@ -49,10 +59,13 @@ Email: <a href="mailto:servicedesk-sao@hamburgsud.com">servicedesk-sao@hamburgsu
 "@
             
             
-#$to
+
+
 #$body
-Send-MailMessage -To $to -Bcc rodrigo.lima-external@alianca.com.br -from Servicedesk-sao@alianca.com.br -Subject $subject -Body $body -BodyAsHtml -SmtpServer smtp-rse.hsdg-ad.int
-#Send-MailMessage -To rodrigo.lima-external@alianca.com.br -from Servicedesk-sao@alianca.com.br -Subject $subject -Body $body -BodyAsHtml -SmtpServer smtp-rse.hsdg-ad.int
+if ($to){
+Send-MailMessage -To $to -Bcc rodrigo.lima-external@alianca.com.br -from Servicedesk-sao@hamburgsud.com -Subject $subject -Body $body -BodyAsHtml -SmtpServer smtp-rse.hsdg-ad.int -Encoding UTF32
+}
+#Send-MailMessage -To rodrigo.lima-external@alianca.com.br -from Servicedesk-sao@alianca.com.br -Subject $subject -Body $body -BodyAsHtml -SmtpServer smtp-rse.hsdg-ad.int -Encoding UTF32
             
 }
 
